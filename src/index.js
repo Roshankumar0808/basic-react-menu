bad5095f490b0b8626f23c6ea69d01f68eebe534
Roshankumar0808 <roshananand2014@gmail.com>
2024-08-25 20:41:56 +0530

    Unused Lib

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import Footer from "./Components/Footer";
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
